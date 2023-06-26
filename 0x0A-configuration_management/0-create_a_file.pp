c7797479549c9775667e97389edf61ed6e467713
2oothpick <kobbyboateng14@gmail.com>
2023-06-26 15:44:53 +0000

    syntax for creating a file wit puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,16 +1,8 @@
-/* 
-resourcetype{'title':
-attribute_name1 => attribute_value
-attribute_name2 => attribute_value
-}
-*/
-
 file {'/tmp/school':
-  path => '/tmp/school',
-  ensure => "present",
-  owner => "www-data",
-  group => "www-data",
-  mode => '0744',
+  ensure  => 'present',
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
   Content => 'I love Puppet',
-  
 }
