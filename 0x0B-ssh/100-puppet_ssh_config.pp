d14855152c1e64367f1f237467997d4e9568ce09
2oothpick <kobbyboateng14@gmail.com>
2023-07-02 02:55:39 +0000

    Add puppet manifest config file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+include stdlib
+
+file_line { 'Turn off password auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delcare private key file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
